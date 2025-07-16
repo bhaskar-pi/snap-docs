2edd3b1145e599c58ebf874e58b09f43fcc98469
bhaskar-pi <bhaskar170863@gmail.com>
2025-07-17 01:30:57 +0530

    add font family

--- app/src/components/auth-layout/index.tsx
+++ app/src/components/auth-layout/index.tsx
@@ -9,13 +9,13 @@ const AuthLayout: React.FC<Props> = ({ children }) => {
   return (
     <div className={`${styles.layout} w-100 h-100 g-0`}>
       <div className="row w-100 h-100 g-0">
-        <div className={`col-md-6 ${styles.container}`}>
+        <div className={`col-md-8 ${styles.container}`}>
           <div className="mb-4">
             <Logo />
           </div>
         </div>
 
-        <div className={`col-md-6 ${styles.children}`}>{children}</div>
+        <div className={`col-md-4 ${styles.children}`}>{children}</div>
       </div>
     </div>
   );
